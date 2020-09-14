302367726ef33bdce782a6a13d2cb4ccab3e229a
P24980 <P24980@UN990155>
2020-09-14 21:32:47 +0100

    converted indentations

--- examples/main.go
+++ examples/main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	rabbit "github.com/PSauerborn/go-jackrabbit"
-	log "github.com/sirupsen/logrus"
+    "fmt"
+    "time"
+    rabbit "github.com/PSauerborn/go-jackrabbit"
+    log "github.com/sirupsen/logrus"
 )
 
 
 func ExampleHandler(msg []byte) {
-	log.Info(fmt.Sprintf("received queue message: %s", string(msg)))
+    log.Info(fmt.Sprintf("received queue message: %s", string(msg)))
 }
 
 func main() {
-	// create new rabbiMQ connection settings and send message over queue
-	config := rabbit.RabbitConnectionConfig{
-		QueueURL: "amqp://guest:guest@192.168.99.100:5672/",
-		QueueName: "new-testing-queue",
-		ExchangeName: "testing-exchange",
-		ExchangeType: "fanout",
-	}
-	go rabbit.ListenOnQueueWithExchange(config, ExampleHandler)
+    // create new rabbiMQ connection settings and send message over queue
+    config := rabbit.RabbitConnectionConfig{
+        QueueURL: "amqp://guest:guest@192.168.99.100:5672/",
+        QueueName: "new-testing-queue",
+        ExchangeName: "testing-exchange",
+        ExchangeType: "fanout",
+    }
+    go rabbit.ListenOnQueueWithExchange(config, ExampleHandler)
 
-	for {
-		err := rabbit.ConnectAndDeliverOverQueue(config, []byte("testing queue message"))
-		if err != nil {
-			log.Error(fmt.Errorf("unable to send message over Rabbit Queue: %v", err))
-		}
-		time.Sleep(5 * time.Second)
-	}
+    for {
+        err := rabbit.ConnectAndDeliverOverQueue(config, []byte("testing queue message"))
+        if err != nil {
+            log.Error(fmt.Errorf("unable to send message over Rabbit Queue: %v", err))
+        }
+        time.Sleep(5 * time.Second)
+    }
 }
--- exchange.go
+++ exchange.go
@@ -1,80 +1,80 @@
 package jackrabbit
 
 import (
-	"fmt"
-	"github.com/streadway/amqp"
-	log "github.com/sirupsen/logrus"
+    "fmt"
+    "github.com/streadway/amqp"
+    log "github.com/sirupsen/logrus"
 )
 
 // helper function used to check for exchange config
 func isValidExchangeConfig(config RabbitConnectionConfig) bool {
-	return config.ExchangeName != "" && config.ExchangeType != ""
+    return config.ExchangeName != "" && config.ExchangeType != ""
 }
 
 // function used to generate new exchange to deliver message over exchange
 func ConnectAndDeliverOverExchange(config RabbitConnectionConfig, payload []byte) error {
-	// connect to rabbitMQ server and generate new channel
-	conn, err := NewRabbitConnection(config)
-	if err != nil {
-		return err
-	}
-	// defer closing of both channel and connection to rabbitMQ server
-	defer conn.Connection.Close()
-	defer conn.Channel.Close()
+    // connect to rabbitMQ server and generate new channel
+    conn, err := NewRabbitConnection(config)
+    if err != nil {
+        return err
+    }
+    // defer closing of both channel and connection to rabbitMQ server
+    defer conn.Connection.Close()
+    defer conn.Channel.Close()
 
-	// check that given configuration has neccessary variables
-	if !isValidExchangeConfig(config) {
-		log.Error("received invalid exchange configuration")
-		return InvalidRabbitMQConfig
-	}
+    // check that given configuration has neccessary variables
+    if !isValidExchangeConfig(config) {
+        log.Error("received invalid exchange configuration")
+        return InvalidRabbitMQConfig
+    }
 
-	// declare events exchange with fanout type
-	err = conn.Channel.ExchangeDeclare(config.ExchangeName, config.ExchangeType, false, false, false, false, nil)
-	if err != nil {
-		log.Error(fmt.Errorf("unable to create rabbitmq exchange: %s", err))
-		return err
-	}
-	// construct new message and send over rabbitMQ channel
-	message := amqp.Publishing{Body: payload}
-	err = conn.Channel.Publish(config.ExchangeName, "", false, false, message)
-	if err != nil {
-		log.Error(fmt.Errorf("unable to send payload over rabbitmq server: %s", err))
-		return err
-	}
-	log.Info(fmt.Sprintf("successfully sent payload %+v over rabbitMQ exchange", message))
-	return nil
+    // declare events exchange with fanout type
+    err = conn.Channel.ExchangeDeclare(config.ExchangeName, config.ExchangeType, false, false, false, false, nil)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to create rabbitmq exchange: %s", err))
+        return err
+    }
+    // construct new message and send over rabbitMQ channel
+    message := amqp.Publishing{Body: payload}
+    err = conn.Channel.Publish(config.ExchangeName, "", false, false, message)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to send payload over rabbitmq server: %s", err))
+        return err
+    }
+    log.Info(fmt.Sprintf("successfully sent payload %+v over rabbitMQ exchange", message))
+    return nil
 }
 
 // function used to generate new exchange to deliver message over exchange
 func ConnectAndDeliverOverDurableExchange(config RabbitConnectionConfig, payload []byte) error {
-	// connect to rabbitMQ server and generate new channel
-	conn, err := NewRabbitConnection(config)
-	if err != nil {
-		return err
-	}
-	// defer closing of both channel and connection to rabbitMQ server
-	defer conn.Connection.Close()
-	defer conn.Channel.Close()
+    // connect to rabbitMQ server and generate new channel
+    conn, err := NewRabbitConnection(config)
+    if err != nil {
+        return err
+    }
+    // defer closing of both channel and connection to rabbitMQ server
+    defer conn.Connection.Close()
+    defer conn.Channel.Close()
 
-	// check that given configuration has neccessary variables
-	if !isValidExchangeConfig(config) {
-		log.Error("received invalid exchange configuration")
-		return InvalidRabbitMQConfig
-	}
+    // check that given configuration has neccessary variables
+    if !isValidExchangeConfig(config) {
+        log.Error("received invalid exchange configuration")
+        return InvalidRabbitMQConfig
+    }
 
-	// declare events exchange with fanout type
-	err = conn.Channel.ExchangeDeclare(config.ExchangeName, config.ExchangeType, true, false, false, false, nil)
-	if err != nil {
-		log.Error(fmt.Errorf("unable to create rabbitmq exchange: %s", err))
-		return err
-	}
-	// construct new message and send over rabbitMQ channel
-	message := amqp.Publishing{Body: payload}
-	err = conn.Channel.Publish(config.ExchangeName, "", false, false, message)
-	if err != nil {
-		log.Error(fmt.Errorf("unable to send payload over rabbitmq server: %s", err))
-		return err
-	}
-	log.Info(fmt.Sprintf("successfully sent payload %+v over rabbitMQ exchange", message))
-	return nil
+    // declare events exchange with fanout type
+    err = conn.Channel.ExchangeDeclare(config.ExchangeName, config.ExchangeType, true, false, false, false, nil)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to create rabbitmq exchange: %s", err))
+        return err
+    }
+    // construct new message and send over rabbitMQ channel
+    message := amqp.Publishing{Body: payload}
+    err = conn.Channel.Publish(config.ExchangeName, "", false, false, message)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to send payload over rabbitmq server: %s", err))
+        return err
+    }
+    log.Info(fmt.Sprintf("successfully sent payload %+v over rabbitMQ exchange", message))
+    return nil
 }
